d461be02871b48193a598bba215e88fc1d11857b
Oskar Korczak <oskar.korczak@protonmail.com>
2023-06-13 22:58:21 +0200

    make short_url field read only

--- urlshortener/shortener/serializers.py
+++ urlshortener/shortener/serializers.py
@@ -4,6 +4,11 @@ from .models import ShortenedURL
 
 
 class ShortenedURLSerializer(serializers.ModelSerializer):
+    short_url = serializers.SerializerMethodField()
+
     class Meta:
         model = ShortenedURL
         fields = ["original_url", "short_url"]
+
+    def get_short_url(self, instance):
+        return instance.short_url
