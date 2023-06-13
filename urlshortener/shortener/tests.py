9f131f2272f132117e84d203b2069849ab0003f0
Oskar Korczak <oskar.korczak@protonmail.com>
2023-06-13 22:35:31 +0200

    added tests

--- urlshortener/shortener/tests.py
+++ urlshortener/shortener/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import ShortenedURL
+
+
+class ShortenedURLTests(APITestCase):
+    def test_create_shortened_url(self):
+        """
+        Ensure we can create a new shortened URL object.
+        """
+        url = reverse("create_shortened_url")
+        data = {"original_url": "http://example.com/very-very/long/url/even-longer"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ShortenedURL.objects.count(), 1)
+        self.assertEqual(
+            ShortenedURL.objects.get().original_url,
+            "http://example.com/very-very/long/url/even-longer",
+        )
+
+    def test_retrieve_original_url(self):
+        """
+        Ensure we can retrieve the original URL from a shortened URL.
+        """
+        # First, create a new shortened URL object.
+        shortened_url = ShortenedURL.objects.create(
+            original_url="http://example.com/very-very/long/url/even-longer"
+        )
+        # Now, retrieve it.
+        url = reverse("retrieve_original_url", args=[shortened_url.short_url])
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["original_url"],
+            "http://example.com/very-very/long/url/even-longer",
+        )
