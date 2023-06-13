929c6f0363a32897f866e200e21f92e9d7ecf321
Oskar Korczak <oskar.korczak@protonmail.com>
2023-06-13 22:42:58 +0200

    using secret from env instead of hardcoded value

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-74x=e77ivl*&b!l^j)!z4h%#t^&_)=mcafi_r0r43g$#qnf8*h"
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
